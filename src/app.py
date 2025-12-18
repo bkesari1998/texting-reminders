df81613a27a143fe82a039073132ae015b93b30c
Bharat Kesari <bkesari1998@gmail.com>
2025-12-17 22:04:15 -0500

    Created Task class
    Task includes a description and a unique_id. Added logic in app.py to test use of Task class.

--- src/app.py
+++ src/app.py
@@ -1,2 +1,17 @@
+from task import Task
+
 if __name__ == '__main__':
-    print("Hello, World")
+    task_0 = Task("pick up dry cleaning")
+    task_1 = Task("brush my teeth", 10)
+
+    # Failure expected
+    try:
+        task_2 = Task("X" * 256)
+    except:
+        print("Unable to create task 2")
+
+    # Failure expected
+    try:
+        task_3 = Task("")
+    except:
+        print("Unable to create task 3")
