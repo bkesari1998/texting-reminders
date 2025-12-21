c443b9cc273915b102d279d35260b6918f22c6f9
Bharat Kesari <bkesari1998@gmail.com>
2025-12-21 12:50:58 -0500

    Created simple DB wrapper class to execute SQLite commands

---
+++ src/db.py
@@ -0,0 +1,43 @@
+import sqlite3
+from task import Task
+
+class TaskDatabase:
+    TASK_TABLE_NAME = "tasks"
+    
+    def __init__(self, path: str):
+        self.path = path
+        
+    def connect(self):
+        self._conn = sqlite3.connect(self.path)
+        self._cursor = self._conn.cursor()
+
+    def disconnect(self):
+        self._conn.close()
+
+    def create_tasks_table(self):
+        self._cursor.execute(f"""
+            CREATE TABLE {self.TASK_TABLE_NAME}(description TEXT)""")
+
+    def add_task(self, task: Task):
+        self._cursor.execute(f"""
+            INSERT INTO {self.TASK_TABLE_NAME} VALUES('{task.description}')""")
+
+    def get_tasks(self) -> list[Task]:
+        self._cursor.execute(f"""
+            SELECT rowid, description FROM {self.TASK_TABLE_NAME}""")
+        
+        tasks = []
+        result = self._cursor.fetchall()
+        for row_id, description in result:
+            tasks.append(Task(description, row_id))
+
+        return tasks
+
+    def get_task(self, row_id: int) -> Task:
+        self._cursor.execute(f"""
+            SELECT rowid, description FROM {self.TASK_TABLE_NAME} WHERE rowid == {row_id}""")
+
+        result = self._cursor.fetchall()
+        row_id, description = result[0]
+
+        return Task(description, row_id)
