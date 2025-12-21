8f6247c0c67afdbd27d4c4630cd1f999fab7ce44
Bharat Kesari <bkesari1998@gmail.com>
2025-12-21 12:51:59 -0500

    Added code to test DB class

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from task import Task
+from db import TaskDatabase
 
 if __name__ == '__main__':
     task_0 = Task("pick up dry cleaning")
@@ -15,3 +16,19 @@ if __name__ == '__main__':
         task_3 = Task("")
     except:
         print("Unable to create task 3")
+
+    # Create database
+    db = TaskDatabase("tasks.db")
+    db.connect()
+    db.create_tasks_table()
+    db.add_task(task_0)
+    db.add_task(Task("finish coding this app"))
+
+    tasks = db.get_tasks()
+    task = db.get_task(1)
+
+    print(tasks)
+    print(task)
+
+    db.disconnect()
+
