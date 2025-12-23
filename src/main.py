f33088a44c47fc93e87126e036d013a6f4b55694
Bharat Kesari <bkesari1998@gmail.com>
2025-12-22 21:52:36 -0500

    Support adding reminders to database via text

--- src/main.py
+++ src/main.py
@@ -1,15 +1,32 @@
 from task import Task
 from db import TaskDatabase
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import Response
+
+from twilio.twiml.messaging_response import MessagingResponse
+from contextlib import asynccontextmanager
+
+db = TaskDatabase("tasks.db")
+db.connect()
+db.create_tasks_table()
 
 app = FastAPI()
 
-@app.get("/hello")
-def hello():
-    return {"message": "hello"}
+@app.post("/sms")
+def sms_handler(Body: str = Form(...)):
+    resp = MessagingResponse()
+    
+    if Body.startswith("Remind me to"):
+        db.add_task(Task(Body[len("Remind me to"):]))
+        msg = resp.message("Ok, I'll remind you to do that!'")
+    else:
+        msg = resp.message("Sorry, I didn't understand that :(")
+
+    return str(resp)
+    
 
 @app.get("/favicon.ico", include_in_schema=False)
 def favicon():
     return Response(status_code=204)
+
