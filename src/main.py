31f567c2953430511a17df410b9793a6cc31b5a9
Bharat Kesari <bkesari1998@gmail.com>
2025-12-22 22:41:17 -0500

    Added feature to text task list to user

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI, Form
 from fastapi.responses import Response
 
 from twilio.twiml.messaging_response import MessagingResponse
-from contextlib import asynccontextmanager
 
 db = TaskDatabase("tasks.db")
 db.connect()
@@ -19,11 +18,22 @@ def sms_handler(Body: str = Form(...)):
     
     if Body.startswith("Remind me to"):
         db.add_task(Task(Body[len("Remind me to"):]))
-        msg = resp.message("Ok, I'll remind you to do that!'")
+        msg = resp.message("Ok, I'll remind you to do that!")
+    elif Body == "What are my tasks?":
+        tasks = db.get_tasks()
+        
+        tasks_string = ""
+        for task in tasks:
+            tasks_string += (str(task) + "\n\n")
+
+        msg = resp.message(tasks_string)
     else:
         msg = resp.message("Sorry, I didn't understand that :(")
 
-    return str(resp)
+    return Response(
+        content=str(resp),
+        media_type="application/xml"
+    )
     
 
 @app.get("/favicon.ico", include_in_schema=False)
