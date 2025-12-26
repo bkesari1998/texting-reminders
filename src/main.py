d304549761124b6e6e1fa76ccc0b62cb771bbe58
Bharat Kesari <bkesari1998@gmail.com>
2025-12-26 16:42:03 -0500

    Added more handlers and get_db() function

--- src/main.py
+++ src/main.py
@@ -1,19 +1,61 @@
-from os import environ
-
 from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker, Session
 
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Depends
 from fastapi.responses import Response
 
 from twilio.twiml.messaging_response import MessagingResponse
 
 from db import engine
 from models import Task
+from api import TaskCreate, TaskOut
+
+from typing import Generator
 
 SessionLocal = sessionmaker(bind=engine)
 
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 app = FastAPI()
+
+@app.post("/add-reminder", response_model=TaskOut, status_code=201)
+def add_reminder(
+    new_task: TaskCreate,
+    db: Session = Depends(get_db)
+) -> TaskOut:
+    task = Task(description=new_task.description, deadline=new_task.deadline)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+
+    return task
+
+@app.get("/all-tasks", response_model=list[TaskOut])
+def get_all_tasks(db: Session = Depends(get_db)) -> list[TaskOut]:
+    stmt = select(Task)
+    result = db.execute(stmt)
+    tasks = result.scalars().all()
+
+    return tasks
+    
+@app.get("/task", response_model=TaskOut)
+def get_task(
+    id: int, 
+    db: Session = Depends(get_db)
+) -> TaskOut:
+    stmt = select(Task).where(Task.id == id)
+    result = db.execute(stmt)
+    task = result.scalar_one_or_none()
+
+    if task:
+        return task
+    else:
+        return Response(status_code=404)
 
 @app.post("/sms")
 def sms_handler(Body: str = Form(...)):
