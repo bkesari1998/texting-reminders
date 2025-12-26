687af033aa7edaab7ec3477e4b2e83d53853bfb4
Bharat Kesari <bkesari1998@gmail.com>
2025-12-26 16:43:52 -0500

    Added FastAPI classes for task creation and return

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from db import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, DateTime, func
+from datetime import datetime
 
 class Task(Base):
     DESCRIPTION_MAX_LEN = 255
@@ -9,6 +10,14 @@ class Task(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     description: Mapped[str] = mapped_column(String(DESCRIPTION_MAX_LEN))
+    created_datetime: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+    deadline: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True
+    )
 
     def __repr__(self) -> str:
         return f"Task(id={self.id!r}, description={self.description!r})"
