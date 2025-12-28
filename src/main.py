5c425902445e5b00fadf2ef08e07aab49251144a
Bharat Kesari <bkesari1998@gmail.com>
2025-12-27 21:51:13 -0500

    Added handlers to display HTML and handle form input

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from typing import Annotated
 from datetime import datetime
 from os import environ
+from typing import Annotated
 
 from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker, Session
