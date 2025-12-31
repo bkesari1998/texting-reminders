a6c6ea0de5b08104c4db3c6a5186438682408eb2
Bharat Kesari <bkesari1998@gmail.com>
2025-12-31 00:01:17 -0500

    Created delete button and JS function to send request to delete-task route

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -28,3 +28,34 @@ async function toggleTaskCompletion(taskId) {
         alert('An error occurred. Please try again.');
     }
 }
+
+async function deleteTask(taskId) {
+    try {
+        const response = await fetch(`/delete-task/${taskId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+
+            const url = new URL(window.location.href);
+            const incomplete_tab = document.getElementById("incomplete-tab")
+
+            if (incomplete_tab.classList.contains("active")) {
+                url.searchParams.set('active_tab', 'incomplete');
+            } else {
+                url.searchParams.set('active_tab', 'complete');
+            }
+
+            window.location.href = url.toString();
+        } else {
+            console.error('Failed to toggle task completion');
+            alert('Failed to update task. Please try again.');
+        }
+    } catch (error) {
+        console.error('Error toggling task completion:', error);
+        alert('An error occurred. Please try again.');
+    }
+}
